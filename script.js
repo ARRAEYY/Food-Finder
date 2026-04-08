b392f2a41bbb1b16d2c159d5e8445619f0bfaaba
Adil <adilreyazsk@gmail.com>
2026-04-08 20:30:45 +0530

    Additional features searching,sorting,like-button,darkmode

--- script.js
+++ script.js
@@ -1,25 +1,117 @@
-const API = "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken";
-
 const container = document.querySelector(".container");
+const inputBox = document.getElementById("input");
+const searchBtn = document.getElementById("search");
+const sortSelect = document.getElementById("sort");
+const themeBtn = document.getElementById("theme");
 
-async function getData() {
-    let response = await fetch(API);
-    let data = await response.json();
 
-    displayData(data.meals);
+
+let meals = [];
+
+
+
+async function fetchMeals(searchText = "salad") {
+  container.innerHTML = "<h2>Loading...</h2>";
+
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    );
+
+    const data = await response.json();
+    meals = data.meals ? data.meals : [];
+
+    displayMeals();
+  } catch (error) {
+    container.innerHTML = "<h2>Something went wrong ❌</h2>";
+  }
 }
 
-function displayData(meals) {
-    meals.forEach(meal => {
-        let div = document.createElement("div");
 
-        div.innerHTML = `
-            <img src="${meal.strMealThumb}" width="200">
-            <h3>${meal.strMeal}</h3>
-        `;
+function displayMeals() {
 
-        container.appendChild(div);
-    });
+  if (meals.length === 0) {
+    container.innerHTML = "<h2>No meals found 😢</h2>";
+    return;
+  }
+
+  
+  let mealsToShow = [...meals];
+
+  
+  if (sortSelect.value === "az") {
+    mealsToShow.sort((a, b) =>
+      a.strMeal.localeCompare(b.strMeal)
+    );
+  }
+
+  if (sortSelect.value === "za") {
+    mealsToShow.sort((a, b) =>
+      b.strMeal.localeCompare(a.strMeal)
+    );
+  }
+
+  
+  let html = "";
+
+  mealsToShow.forEach((meal) => {
+    html += `
+      <div class="card">
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+        <h3>${meal.strMeal}</h3>
+        <button onclick="toggleLike('${meal.idMeal}')">
+          ${meal.liked ? "❤️" : "🤍"}
+        </button>
+      </div>
+    `;
+  });
+
+  container.innerHTML = html;
 }
 
-getData();
+
+function toggleLike(id) {
+  meals = meals.map((meal) => {
+    if (meal.idMeal === id) {
+
+      return { ...meal, liked: !meal.liked };
+    } else {
+      return meal;
+    }
+  });
+
+  displayMeals();
+}
+
+
+
+function handleSearch() {
+  const text = inputBox.value.trim();
+
+  if (text === "") {
+    fetchMeals(); 
+  } else {
+    fetchMeals(text);
+  }
+}
+
+
+searchBtn.addEventListener("click", handleSearch);
+
+inputBox.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    handleSearch();
+  }
+});
+
+
+sortSelect.addEventListener("change", displayMeals);
+
+
+themeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
+
+
+
+fetchMeals(); 
